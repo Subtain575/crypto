0b3c3852c4d417c37ecec2df92fd2d98c5d128e5
saqlain-dev007 <saqlain.sparkx@gmail.com>
2025-06-17 20:01:34 +0500

    Enhance ExecuteTradeDto with validation for quantity and price
    - Added Min validation to the quantity and price fields to ensure they are non-negative.
    - Included Type transformation for quantity and price to ensure proper data handling.

--- src/simulatedTrading/dto/trade.dto.ts
+++ src/simulatedTrading/dto/trade.dto.ts
@@ -1,6 +1,7 @@
 // dto/trade.dto.ts
-import { IsString, IsNumber, IsOptional } from 'class-validator';
+import { IsString, IsNumber, IsOptional, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class ExecuteTradeDto {
   @ApiProperty({
@@ -14,14 +15,18 @@ export class ExecuteTradeDto {
     example: 0.5,
     description: 'Quantity of the asset to trade',
   })
+  @Type(() => Number)
   @IsNumber()
+  @Min(0)
   quantity: number;
 
   @ApiProperty({
     example: 27000,
     description: 'Simulated price at which trade is executed',
   })
+  @Type(() => Number)
   @IsNumber()
+  @Min(0)
   price: number;
 
   @ApiProperty({
