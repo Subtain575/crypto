1f8178b68b521315ec888a23228cd7c4b6a8f996
saqlain-dev007 <saqlain.sparkx@gmail.com>
2025-07-10 14:25:27 +0500

    Fix changes in payment intent dto

--- src/subscribe/dto/stripe.dto.ts
+++ src/subscribe/dto/stripe.dto.ts
@@ -1,10 +1,13 @@
 // create-payment-intent.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail } from 'class-validator';
 
 export class CreatePaymentIntentDto {
   @ApiProperty({ example: '64fc96c52a...', description: 'User ID' })
+  @IsString()
   userId: string;
 
   @ApiProperty({ example: 'test@example.com', description: 'User Email' })
+  @IsEmail()
   email: string;
 }
