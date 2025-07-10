dc97a48dfc0faf63b98807284a3e9e34eacef0dc
saqlain-dev007 <saqlain.sparkx@gmail.com>
2025-07-10 12:22:27 +0500

    Fix Import issue

--- src/simulatedTrading/simulated-trading.module.ts
+++ src/simulatedTrading/simulated-trading.module.ts
@@ -14,7 +14,7 @@ import { WalletModule } from '../wallet/wallet.module';
 import { Wallet, WalletSchema } from '../wallet/schema/wallet.schema';
 import { User, UserSchema } from '../auth/schema/user.schema';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
-import { NotificationModule } from '@/notifications/notification.module';
+import { NotificationModule } from '../notifications/notification.module';
 
 @Module({
   imports: [
